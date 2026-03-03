ddf2945bf54f4c22f2b8ef549a977cc33cd1e3e3
Tristan Strauss <tristanzstrauss@gmail.com>
2026-03-03 19:01:28 +0200

    added all on and all off to gpio pins with relevant command

--- new/slave/console.py
+++ new/slave/console.py
@@ -11,12 +11,16 @@ print("[-] Waiting for commands from master...")
 while True:
     if ser.in_waiting > 0:
         command = ser.readline().decode().strip()
-        print(f"[+] Received command from master: {command}")
+        print(f"\n[+] Received command from master: {command}")
         if (command.split("_")[1] == "High"):
             pin = command.split("-")[0]
             gpio.set_pin_high(pin=pin)
         elif (command.split("_")[1] == "Low"):
             pin = command.split("-")[0]
             gpio.set_pin_low(pin=pin)
+        elif (command.split("_")[1] == "ON"):
+            gpio.set_all_low()
+        elif (command.split("_")[1] == "OFF"):
+            gpio.set_all_high()
         else:
             print(f"[!] Unknown command from master")
